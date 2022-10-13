9a3e423a149e401604fd189c52f28063c5d4bc60
Juan Bautista Parra <jbparra84@gmail.com>
2022-10-12 23:16:13 -0300

    Introduction to nodeJS and NPM Module Complete

--- index.js
+++ index.js
@@ -32,17 +32,53 @@ console.log('Will read file!'); */
 
 ////////////////////////////////////////
 ////// SERVER & BASIC API
+
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObj = JSON.parse(data);
+const dataJson = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
+
+    const {query, pathname } = url.parse(req.url, true);
+
+    // Overview page
     if (req.url ==='/' || req.url === '/overview') {
-        res.end('OVERVIEW');
-    } else if (req.url ==='/product') {
-        res.end('PRODUCT');
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const cardsHtml = dataJson.map(el => replaceTemplate(templateCard, el)).join('');
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
+
+    // Product page
+    } else if (pathname === '/product') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const product = dataJson[query.id];
+        const output = replaceTemplate(templateProduct, product);
+        res.end(output);
+
+    // API page
     } else if (req.url === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
+
+    // Not found
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
