cbc0190bbe3391ee359635cd2608550c089cc9d9
Juan Bautista Parra <jbparra84@gmail.com>
2022-10-12 21:31:13 -0300

    Change made in build a very simple api, video class

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const http = require('http');
+const path = require('node:path/win32');
+const url = require('url');
 
 ////////////////////////////////////////
 ////// FILES 
@@ -30,8 +32,24 @@ console.log('Will read file!'); */
 
 ////////////////////////////////////////
 ////// SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server!');
+    if (req.url ==='/' || req.url === '/overview') {
+        res.end('OVERVIEW');
+    } else if (req.url ==='/product') {
+        res.end('PRODUCT');
+    } else if (req.url === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'Hello World'
+        });
+        res.end('<h1>Page not FOUND!</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
