3e5ead6c940dccbea1faa3046d28a136184cd27c
Juan Bautista Parra <jbparra84@gmail.com>
2022-10-13 00:06:33 -0300

    add nodemon

--- index.js
+++ index.js
@@ -3,49 +3,9 @@ const http = require('http');
 const path = require('node:path/win32');
 const url = require('url');
 
-////////////////////////////////////////
-////// FILES 
-// Blocking, Synchronous Way
-/* const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+const slugify = require('slugify');
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File has benn written!'); */
-
-// Non Blocking, Asynchronous Way
-/* fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    if (err) return console.log('Error!');
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            console.log(data3);
-
-            fs.writeFile('./text/final.txt', `${data2}\n${data3}`, 'utf-8', (err) =>{
-                console.log("The file has been written! :D");
-            });
-        });
-    });
-});
-
-console.log('Will read file!'); */
-
-////////////////////////////////////////
-////// SERVER & BASIC API
-
-const replaceTemplate = (template, product) => {
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
+const replaceTemplate = require(`${__dirname}/modules/replaceTemplate`);
 
 
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
@@ -54,6 +14,9 @@ const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataJson = JSON.parse(data);
+
+const slug = dataJson.map(el => slugify(el.productName, {lower: true}));
+console.log(slug);
 
 const server = http.createServer((req, res) => {
 
